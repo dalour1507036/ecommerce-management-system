e9e65c6cdd95d116c4a74f0c1baa0d13fd14dc3f
saeed.dalour <saeed.dalour@gmail.com>
2024-05-10 18:25:26 +0600

    Add controller for max-sale-day-from-range API

--- src/main/java/com/dalourhossan/ecommerce/controller/api/v1/V1SaleController.java
+++ src/main/java/com/dalourhossan/ecommerce/controller/api/v1/V1SaleController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/api/v1/sales")
 public class V1SaleController {
@@ -21,5 +23,16 @@ public class V1SaleController {
     public ResponseEntity<Double> getTotalSaleAmountOfCurrentDay() {
         double totalSaleAmount = saleService.getTotalSaleAmountOfCurrentDay();
         return ResponseEntity.status(HttpStatus.OK).body(totalSaleAmount);
+    }
+
+    @GetMapping("/max-sale-day")
+    public ResponseEntity<LocalDate> getMaxSaleDay(@RequestParam("startDate") String startDateStr,
+                                                   @RequestParam("endDate") String endDateStr) {
+        startDateStr = startDateStr.trim();
+        endDateStr = endDateStr.trim();
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
+        LocalDate maxSaleDay = saleService.getMaxSaleDay(startDate, endDate);
+        return ResponseEntity.status(HttpStatus.OK).body(maxSaleDay);
     }
 }
