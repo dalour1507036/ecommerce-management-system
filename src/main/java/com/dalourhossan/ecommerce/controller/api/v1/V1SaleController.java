b17727f83a7e98a229bd3629634c124c65e55cbe
saeed.dalour <saeed.dalour@gmail.com>
2024-05-10 18:57:59 +0600

    Add controller for top-selling-items API

--- src/main/java/com/dalourhossan/ecommerce/controller/api/v1/V1SaleController.java
+++ src/main/java/com/dalourhossan/ecommerce/controller/api/v1/V1SaleController.java
@@ -1,5 +1,7 @@
 package com.dalourhossan.ecommerce.controller.api.v1;
 
+import com.dalourhossan.ecommerce.entity.Product;
+import com.dalourhossan.ecommerce.service.OrderItemService;
 import com.dalourhossan.ecommerce.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,16 +9,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sales")
 public class V1SaleController {
     private final SaleService saleService;
+    private final OrderItemService orderItemService;
+
 
     @Autowired
-    public V1SaleController(SaleService saleService) {
+    public V1SaleController(SaleService saleService, OrderItemService orderItemService) {
         this.saleService = saleService;
-
+        this.orderItemService = orderItemService;
     }
 
     @GetMapping("/total-amount")
@@ -34,5 +39,12 @@ public class V1SaleController {
         LocalDate endDate = LocalDate.parse(endDateStr);
         LocalDate maxSaleDay = saleService.getMaxSaleDay(startDate, endDate);
         return ResponseEntity.status(HttpStatus.OK).body(maxSaleDay);
+    }
+
+    @GetMapping("top-selling-products/{limit}")
+    public ResponseEntity<List<Product>> getTopSellingItems(@PathVariable int limit) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                orderItemService.getTopSellingItems(limit)
+        );
     }
 }
