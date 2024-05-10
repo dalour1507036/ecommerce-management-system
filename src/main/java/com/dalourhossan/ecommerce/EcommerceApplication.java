4bf990da5bf5f8713dc1a6a06910ac1c3147ec9b
saeed.dalour <saeed.dalour@gmail.com>
2024-05-10 18:04:18 +0600

    update main and configuration file

--- src/main/java/com/dalourhossan/ecommerce/EcommerceApplication.java
+++ src/main/java/com/dalourhossan/ecommerce/EcommerceApplication.java
@@ -3,7 +3,7 @@ package com.dalourhossan.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dalourhossan.ecommerce")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
